0c86fde7d311db813e8b56ce99ad481821f706b4
Tim McLean <timmyclean@yahoo.ca>
2021-04-10 11:48:14 -0400

    added comments

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,21 +1,27 @@
+# using library function
 library(dplyr)
 
+# Importing i used  the import dataset button
 MechaCar_mpg <- read.csv("~/MechaCarChallenge.Rscript/MechaCar_mpg.csv")
 
 View(MechaCar_mpg)
 
-Summary(lm(vehicle_length~vehicle_weight,spoiler_angle,ground_clearance,AWD,mpg))
-
+# linear regression using lm
 mecha_lm <- lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
 
+# summary to determine p value and r squared value
 summary(mecha_lm)
 
+# import the other data set
 Suspension_Coil <- read.csv("~/MechaCarChallenge.Rscript/Suspension_Coil.csv")
 
+# summarize mean, median, variance and standard D.
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
+# use group_by for each lot
 lot_sumamry <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD=sd(PSI),groups='keep')
 
+# t tests!
 t.test(Suspension_Coil$PSI,mu = 1500)
 
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
